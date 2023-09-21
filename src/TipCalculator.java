b6aac11f157acfcdccb5a94b997f8d7e64bfa610
Guo Feng He <guofengh@nycstudents.net>
2023-09-21 18:45:20 -0400

    added a feature that allows users to input item names corresponding to the cost they inputted. item names then get printed with the receipt at the end.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -12,13 +12,21 @@ public class TipCalculator{
         System.out.println();
         double itemCost = 0;
         double totalCost = 0;
+        StringBuilder itemList = new StringBuilder();
 
-        while (itemCost != -1){
+        while (itemCost != -1) {
             System.out.print("Enter a cost in dollars and cents, (e.g. 12.50) -1 to end: ");
             itemCost = scan.nextDouble();
+            scan.nextLine();
             totalCost += itemCost;
+            if (itemCost != -1) {
+                System.out.print("Enter item name: ");
+                String itemName = scan.nextLine();
+                itemList.append(itemName).append("\n"); // adding spaces courtesy of: https://stackoverflow.com/questions/7833689/how-can-i-print-a-string-adding-newlines-in-java
+            }
         }
 
+        scan.close();
         totalCost += 1;
         double tip = (double) tipPercentage / 100 * totalCost;
         System.out.println("----------------------");
@@ -29,6 +37,8 @@ public class TipCalculator{
         System.out.println("Per person cost before tip: $" + String.format("%.2f",(totalCost / peopleEating)));
         System.out.println("Tip per person: $" + String.format("%.2f", (tip / peopleEating)));
         System.out.println("Total cost per person: $" + String.format("%.2f",(totalCost / peopleEating) + (tip / peopleEating)));
-
+        System.out.println("------------------------");
+        System.out.println("Items Ordered:");
+        System.out.println(itemList);
     }
 }
