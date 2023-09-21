912b169ecaa06d4f0cd3f116674e9faf096d88e9
BT_2N2_27 <sample@bths.edu>
2023-09-21 13:22:59 -0400

    added a while loop to add costs. Working on the receipt

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -9,6 +9,22 @@ public class TipCalculator{
         int peopleEating = scan.nextInt();
         System.out.print("Tip Percentage? (as an int): ");
         int tipPercentage = scan.nextInt();
+        System.out.println();
+        double itemCost = 0;
+        double totalCost = 0;
+
+        while (itemCost != -1){
+            System.out.print("Enter a cost in dollars and cents, (e.g. 12.50) -1 to end: ");
+            itemCost = scan.nextDouble();
+            totalCost += itemCost;
+        }
+
+        double tip = (double) tipPercentage / 100 * totalCost;
+        System.out.println("----------------------");
+        System.out.println("Total bill before tip: " + totalCost);
+        System.out.println("Total Percentage: " + tipPercentage + "%");
+        System.out.println("Total Tip: " + ((double) tipPercentage / 100) * totalCost);
+        System.out.println("Total bill with tip: " + (1 + (double) tipPercentage / 100) * totalCost);
 
     }
 }
